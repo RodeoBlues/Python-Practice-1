d3d4ce41716ad9502bc3e7c5abf090467a92ef9e
vimalraj571 <vimalraj.ktm95@gmail.com>
2018-06-10 20:34:44 +0530

    Some additions

---
+++ randoms/Bodacious_uda.py
@@ -0,0 +1,12 @@
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+
+s_break = s[0:3]
+t_break = t[4:]
+
+print(s_break+t_break)
---
+++ randoms/week-day.py
@@ -0,0 +1,5 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+week=7
+day=24
+print (week*week)*day
