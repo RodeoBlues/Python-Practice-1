194df1b66300f85cead5e42b864f9bbd17919d47
vimalraj571 <vimalraj.ktm95@gmail.com>
2018-06-16 17:01:21 +0530

    New addtion

--- randoms/GUV/set1/ar_sum.py
+++ randoms/GUV/set1/ar_sum.py
@@ -1,7 +1,10 @@
-n,total=map(int,input().split())
-e = list(map(int,input().split()))
-result = 0
-while total > 0:
-	result = result + e[total]
-	total -= 1
-print(result)
+def array_sum():
+	n,total=map(int,input().split())
+	e = list(map(int,input().split()))
+	out=0
+	while total>0:
+		out += e[total-1]
+		total-=1
+	print(out)
+
+array_sum()
